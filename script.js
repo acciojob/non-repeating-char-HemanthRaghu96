ca2eeb6cbc7456b33fa561bd6e798bf85ec65800
Hemanth R <91587889+HemanthRaghu96@users.noreply.github.com>
2025-11-29 19:26:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+  // Write your code here
+  let obj = {};
+  for (let i = 0; i < str.length; i++) {
+    if (obj[str[i]]) {
+      obj[str[i]]++;
+    } else {
+      obj[str[i]] = 1;
+    }
+  }
+  let key = Object.keys(obj);
+  let value = Object.values(obj);
+  for (let i = 0; i < value.length; i++) {
+    if (value[i] == 1) {
+      return key[i];
+    } 
+  }
+   return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
